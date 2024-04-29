2b3de9cd7c2a23e049ca87c156b6edfd381861c2
daloir <s222927@dtu.dk>
2024-04-29 16:15:32 +0200

    updated pipeline

--- util/match_labels.py
+++ util/match_labels.py
@@ -69,78 +69,78 @@ def match_emotion_labels(labels_file, corrected_labels_file, directory, dataset=
     print()
 
 
+    if dataset == "MELD":
+        # store a list of files that do not have labels
+        files_without_labels = files.copy()
+        # iterate through the column 'Expected filename' and check if any filenames are not in 'files'
+        for index, row in data.iterrows():
+            if row['Expected filename'] in files:
+                # remove "expected filename" from 'files'
+                files_without_labels.remove(row['Expected filename'])
+            
+            # if row['Expected filename'] not in files:
+                # files_without_labels.append(row['Expected filename'])
 
-    # store a list of files that do not have labels
-    files_without_labels = files.copy()
-    # iterate through the column 'Expected filename' and check if any filenames are not in 'files'
-    for index, row in data.iterrows():
-        if row['Expected filename'] in files:
-            # remove "expected filename" from 'files'
-            files_without_labels.remove(row['Expected filename'])
-        
-        # if row['Expected filename'] not in files:
-            # files_without_labels.append(row['Expected filename'])
+        # print()
+        print("Files without labels:")
+        print(files_without_labels)
+        print()
 
-    # print()
-    print("Files without labels:")
-    print(files_without_labels)
-    print()
+        # remove files that do not have labels from 'files' and delete them from the directory
+        deleted_files = 0
+        for file in files_without_labels:
+            # delete audio file drom directory
+            os.remove(os.path.join(directory, file))
+            deleted_files += 1
+            files.remove(file)
+        # print()
+        print("Deleted files: " + str(deleted_files))
+        print()
 
-    # remove files that do not have labels from 'files' and delete them from the directory
-    deleted_files = 0
-    for file in files_without_labels:
-        # delete audio file drom directory
-        os.remove(os.path.join(directory, file))
-        deleted_files += 1
-        files.remove(file)
-    # print()
-    print("Deleted files: " + str(deleted_files))
-    print()
+        # print(files)
+        # print()
 
-    # print(files)
-    # print()
-
-    # Check if there are any duplicate files in 'files'
-    # duplicates = set([x for x in files if files.count(x) > 1])
-    # print("Duplicates:")
-    # print(duplicates)
-    # print()
+        # Check if there are any duplicate files in 'files'
+        # duplicates = set([x for x in files if files.count(x) > 1])
+        # print("Duplicates:")
+        # print(duplicates)
+        # print()
 
 
-    # lenght of data BEFORE
-    # print()
-    print("Length of data BEFORE:")
-    print(len(data))
-    print()
+        # lenght of data BEFORE
+        # print()
+        print("Length of data BEFORE:")
+        print(len(data))
+        print()
 
-    # remove dataframe rows if file is not in 'files'
-    print("Removing rows from dataframe if file is not in 'files'...")
-    print()
-    data = data[data['Expected filename'].isin(files)]
+        # remove dataframe rows if file is not in 'files'
+        print("Removing rows from dataframe if file is not in 'files'...")
+        print()
+        data = data[data['Expected filename'].isin(files)]
 
-    # lenght of data AFTER
-    # print()
-    print("Length of data AFTER:")
-    print(len(data))
-    print()
+        # lenght of data AFTER
+        # print()
+        print("Length of data AFTER:")
+        print(len(data))
+        print()
 
-    files.sort()
+        files.sort()
 
-    # Add filenames to a new column in the DataFrame
-    data['filename'] = files
+        # Add filenames to a new column in the DataFrame
+        data['filename'] = files
 
-    # Iterate through dataframe and check if any filenames do not match the expected filenames
-    num_missmatches = 0
-    for index, row in data.iterrows():
-        if row['filename'] != row['Expected filename']:
-            print(row['filename'])
-            print(row['Expected filename'])
-            print()
-            num_missmatches += 1
+        # Iterate through dataframe and check if any filenames do not match the expected filenames
+        num_missmatches = 0
+        for index, row in data.iterrows():
+            if row['filename'] != row['Expected filename']:
+                print(row['filename'])
+                print(row['Expected filename'])
+                print()
+                num_missmatches += 1
 
-    # print()
-    print("Number of missmatches: " + str(num_missmatches))
-    print()
+        # print()
+        print("Number of missmatches: " + str(num_missmatches))
+        print()
 
     # Export dataframe to csv
     # data.to_csv(r"C:\MyDocs\DTU\MSc\Thesis\Data\MELD\MELD_fine_tune_v1_train_data\export_csv.csv", index=False)
@@ -158,6 +158,9 @@ def match_emotion_labels(labels_file, corrected_labels_file, directory, dataset=
 
     if dataset == "MELD":
         my_encoding_dict = {'anger': 0, 'disgust': 1, 'fear': 2, 'joy': 3, 'neutral': 4, 'sadness': 5, 'surprise': 6}
+    elif dataset == "CREMA-D":
+        my_encoding_dict = {'ANG': 0, 'DIS': 1, 'FEA': 2, 'HAP': 3, 'NEU': 4, 'SAD': 5}
+    
     
     labels = data['Emotion'].map(my_encoding_dict).values
 
@@ -241,7 +244,10 @@ def match_emotion_labels(labels_file, corrected_labels_file, directory, dataset=
 
     # Drop columns Sr No., Utterance, Speaker, Sentiment, Dialogue_ID, Utterance_ID, Season, Episode, StartTime, EndTime, Expected filename, Match
     # data = data.drop(['Sr No.', 'Utterance', 'Speaker', 'Sentiment', 'Dialogue_ID', 'Utterance_ID', 'Season', 'Episode', 'StartTime', 'EndTime', 'Expected filename', 'Match'], axis=1)
-    # data = data.drop(['Sr No.', 'Utterance', 'Speaker', 'Sentiment', 'Dialogue_ID', 'Utterance_ID', 'Season', 'Episode', 'StartTime', 'EndTime', 'Expected filename'], axis=1)
+    if dataset == "MELD":
+        data = data.drop(['Sr No.', 'Utterance', 'Speaker', 'Sentiment', 'Dialogue_ID', 'Utterance_ID', 'Season', 'Episode', 'StartTime', 'EndTime', 'Expected filename'], axis=1)
+    elif dataset == "CREMA-D":
+        data = data.drop(['Index', 'Speaker', 'Line', 'Intensity'], axis=1)
 
     # data.reset_index(drop=True, inplace=True)
 
