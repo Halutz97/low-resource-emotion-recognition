679c349a5bbf2b0b9ed80cb64a5c0534b536511d
Halutz97 <ari2626@gmail.com>
2024-05-02 16:41:27 +0200

    Updated audio_dataset

--- audio_dataset.py
+++ audio_dataset.py
@@ -6,13 +6,14 @@ from torch.utils.data import Dataset
 from transformers import Wav2Vec2Processor, Wav2Vec2Model
 
 class AudioDataset(Dataset):
-    def __init__(self, directory):
+    def __init__(self, directory, processor):
         """
         Args:
             directory (string): Directory with all the .wav files.
         """
         self.directory = directory
         self.file_names = [file for file in os.listdir(directory) if file.endswith('.wav')]
+        self.processor = processor
 
     def __len__(self):
         return len(self.file_names)
@@ -27,7 +28,7 @@ class AudioDataset(Dataset):
             waveform = resampler(waveform)
 
         # Process the waveform through the wav2vec2 processor
-        inputs = processor(waveform.squeeze(0), sampling_rate=16000, return_tensors="pt").input_values
+        inputs = self.processor(waveform.squeeze(0), sampling_rate=16000, return_tensors="pt").input_values
 
         # Optionally, you could add other transformations here (e.g., feature extraction)
         # return waveform, 16000  # Return the resampled waveform and the new sample rate
