df43506bdfc80339a78f5ed56a65a10bbb49d314
Halutz97 <ari2626@gmail.com>
2024-06-04 10:14:08 +0200

    updated visual model in multimodal modules for colab

--- multimodal_modules/visual_model.py
+++ multimodal_modules/visual_model.py
@@ -1,12 +1,12 @@
 import get_face_areas
 from get_models import load_weights_EE, load_weights_LSTM
 import sequences
-import os
-import time
-import pickle
+# import os
+# import time
+# import pickle
 import numpy as np
-from scipy import stats
-import pandas as pd
+# from scipy import stats
+# import pandas as pd
 # from ryumina_fer_model.select_video_subset import select_video_subset
 
 # define class
@@ -33,7 +33,7 @@ class VisualModel:
         conf_d = 0.7
         # backbone_model_path = 'ryumina_fer_model/models_fer/EmoAffectnet/weights_0_66_37_wo_gl.h5'
         # LSTM_model_path = 'ryumina_fer_model/models_fer/LSTM/CREMA-D_with_config.h5'
-        emotion_dict = {"NEU": "Neutral", "HAP": "Happiness", "SAD": "Sadness", "SUR": "Surprise", "FEA": "Fear", "DIS": "Disgust", "ANG": "Anger"}
+        # emotion_dict = {"NEU": "Neutral", "HAP": "Happiness", "SAD": "Sadness", "SUR": "Surprise", "FEA": "Fear", "DIS": "Disgust", "ANG": "Anger"}
         # true_label = emotion_dict[video_file.split("_")[2]]
 
         # start_time = time.time()
@@ -53,25 +53,10 @@ class VisualModel:
         step_size = 5
         window_size = 10
         seq_paths, seq_features = sequences.sequences(name_frames, features, win=window_size, step=step_size)
-        # print("Step size: ", step_size)
-        # print("Window size: ", window_size)
-        # print("Sampled frames ", list(range(0,len(name_frames)+1,step_size)))
-        # print("Number of steps/windows: ", len(seq_paths))
         pred = LSTM_model(np.stack(seq_features)).numpy()
-
-        # let's inspect the characteristics of the prediction
-        # print("Pred shape")
-        # print(pred.shape)
-        # print("Pred")
-        # print(pred)
 
         # Take the average of all len(seq_paths) predictions
         avg_pred = np.mean(pred, axis=0)
-         # Let's inspect the characteristics of the average prediction
-        # print("Avg pred shape")
-        # print(avg_pred.shape)
-        # print("Avg pred")
-        # print(avg_pred)
 
         out_prob = avg_pred
         # Get the max of the average prediction, and the index of the max
@@ -81,23 +66,6 @@ class VisualModel:
         # Get the text label of the max
         text_lab = label_model[index]
 
-        # print results
-        # print("out_prob")
-        # print(out_prob)
-        # print("score")
-        # print(score)
-        # print("index")
-        # print(index)
-        # print("text_lab")
-        # print(text_lab)
-
         return out_prob, score, index, text_lab
-
-if __name__ == "__main__":
-    root_path = r"C:\MyDocs\DTU\MSc\Thesis\Data\CREMA-D\CREMA-D\TEST\HI"
-    video_file = select_video_subset(root_path,1)
-    video_path = os.path.join(root_path, video_file[0])
-    my_classifier = VisualModel()
-    my_classifier.classify_video_file(video_path)
 
 
